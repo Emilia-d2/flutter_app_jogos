cd57cba0fd690d0ecd371db26f2c0497812891c2
Emilia-d2 <emiliaferlin18@gmail.com>
2022-12-29 15:25:47 -0300

    chore: ajustes

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'App zoeira',
+      title: 'App dos jogos',
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
